ad2ea36171a1214c7f4a11fff2dbba7a310c2a04
Pyolsuni <tristan@glehen.fr>
2024-01-27 18:12:38 +0100

    Start of scene decoration

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
     public static Counter Instance;
+
+    public Slider Laughbar;
+
+    public float sliderVelocity = 0;
 
     private int combo;
 
@@ -32,6 +37,7 @@ public class Counter : MonoBehaviour
         set
         {
             score = Mathf.Min(value,100);
+            score = Mathf.Max(score, 0);
             HUDBar.instance.SetScore(score);
         }
     }
@@ -39,11 +45,17 @@ public class Counter : MonoBehaviour
     void Start()
     {
         Combo = 0;
-        Score = 50;
+        Score = 20;
     }
 
     private void Awake()
     {
         Instance = this;
+    }
+
+    void Update()
+    {
+        float currentScore = Mathf.SmoothDamp(Laughbar.value, Score, ref sliderVelocity, 100 * Time.deltaTime);
+        Laughbar.value = currentScore;
     }
 }
--- Assets/Scripts/HUDBar.cs
+++ Assets/Scripts/HUDBar.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class HUDBar : MonoBehaviour
 {
