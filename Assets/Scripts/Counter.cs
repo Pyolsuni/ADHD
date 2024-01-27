9ea7155d34087fbcf6387f7ab774162c25e6f572
Pyolsuni <tristan@glehen.fr>
2024-01-27 21:02:39 +0100

    Gameplay Before implementing animations

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -13,6 +13,18 @@ public class Counter : MonoBehaviour
 
     private int combo;
 
+    public GameObject Jester0;
+    public GameObject Jester3;
+    public GameObject Jester5;
+    public GameObject Jester8;
+
+    public GameObject Queen0;
+    public GameObject Queen1;
+    public GameObject Queen2;
+    public GameObject Queen3;
+    public GameObject Queen4;
+    public GameObject Queen5;
+
     public int Combo
     {
         get
@@ -45,7 +57,7 @@ public class Counter : MonoBehaviour
     void Start()
     {
         Combo = 0;
-        Score = 20;
+        Score = 30;
     }
 
     private void Awake()
@@ -57,5 +69,92 @@ public class Counter : MonoBehaviour
     {
         float currentScore = Mathf.SmoothDamp(Laughbar.value, Score, ref sliderVelocity, 100 * Time.deltaTime);
         Laughbar.value = currentScore;
+
+
+        //Joke-ster positions
+        if(Combo < 5)
+        {
+            Jester0.SetActive(true);
+            Jester3.SetActive(false);
+            Jester5.SetActive(false);
+            Jester8.SetActive(false);
+        }
+        else if (Combo < 10)
+        {
+            Jester0.SetActive(false);
+            Jester3.SetActive(true);
+            Jester5.SetActive(false);
+            Jester8.SetActive(false);
+        }
+        else if (Combo < 15)
+        {
+            Jester0.SetActive(false);
+            Jester3.SetActive(false);
+            Jester5.SetActive(true);
+            Jester8.SetActive(false);
+        }
+        else
+        {
+            Jester0.SetActive(false);
+            Jester3.SetActive(false);
+            Jester5.SetActive(false);
+            Jester8.SetActive(true);
+        }
+
+        //Queen positions
+        if (Score < 15)
+        {
+            Queen0.SetActive(true);
+            Queen1.SetActive(false);
+            Queen2.SetActive(false);
+            Queen3.SetActive(false);
+            Queen4.SetActive(false);
+            Queen5.SetActive(false);
+        }
+        else if (Score < 25) 
+        { 
+            Queen0.SetActive(false);
+            Queen1.SetActive(true);
+            Queen2.SetActive(false);
+            Queen3.SetActive(false);
+            Queen4.SetActive(false);
+            Queen5.SetActive(false);
+        }
+        else if (Score < 45)
+        {
+            Queen0.SetActive(false);
+            Queen1.SetActive(false);
+            Queen2.SetActive(true);
+            Queen3.SetActive(false);
+            Queen4.SetActive(false);
+            Queen5.SetActive(false);
+        }
+        else if (Score < 65)
+        {
+            Queen0.SetActive(false);
+            Queen1.SetActive(false);
+            Queen2.SetActive(false);
+            Queen3.SetActive(true);
+            Queen4.SetActive(false);
+            Queen5.SetActive(false);
+        }
+        else if (Score < 85)
+        {
+            Queen0.SetActive(false);
+            Queen1.SetActive(false);
+            Queen2.SetActive(false);
+            Queen3.SetActive(false);
+            Queen4.SetActive(true);
+            Queen5.SetActive(false);
+        }
+        else
+        {
+            Queen0.SetActive(false);
+            Queen1.SetActive(false);
+            Queen2.SetActive(false);
+            Queen3.SetActive(false);
+            Queen4.SetActive(false);
+            Queen5.SetActive(true);
+        }
     }
 }
